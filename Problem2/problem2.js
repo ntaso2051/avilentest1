a00d1d930446edb0300a5f0cb122c08280d45b52
ntaso <ntaso@macbookpro.AirPort>
2021-10-04 17:10:45 +0900

    modified problem2

--- Problem2/problem2.js
+++ Problem2/problem2.js
@@ -16,9 +16,9 @@ reader.on('close', () => {
   var t = strs[0];
   for (var i = 0; i < t; i++) {
     var li = strs[i + 1].split(' ');
-    var a = li[0];
-    var b = li[1];
-    var c = li[2];
+    var a = Number(li[0]);
+    var b = Number(li[1]);
+    var c = Number(li[2]);
     if (
       (a > c && c > b) ||
       (b > a && a > c) ||
@@ -26,21 +26,50 @@ reader.on('close', () => {
       (c > a && a > b)
     ) {
       console.log(0);
-    } else {
-      var cnt = 0;
-      var mn = 0;
-      var eq = 0;
-      if (a == c) {
-        eq = 1;
+    } else if (b > a && b > c) {
+      if (a == c && a != 1) {
+        console.log(1);
+      } else {
+        console.log(-1);
       }
-      mn = Math.min(a, c);
-      mn -= eq;
-      cnt = b - mn + 1;
-      b -= cnt;
-      cnt += eq;
-      if (mn <= 0 || b <= 0) cnt = -1;
+    } else if (a > b && c > b) {
+      if (a - 1 != b) {
+        console.log(1);
+      } else {
+        if (a - 3 <= 0) {
+          console.log(-1);
+        } else {
+          console.log(5);
+        }
+      }
+    } else if (a == c && b == a) {
+      if (a - 2 <= 0) {
+        console.log(-1);
+      } else {
+        console.log(3);
+      }
+    } else {
+      var mn = Math.min(a, c);
+      var mx = Math.max(a, c);
+      var cnt1 = b - mn + 1;
+      var cnt2 = mx - b + 1;
+      if (mn - 1 <= 0) {
+        cnt1 = -2;
+      }
+      if (b - 1 == mn && mn - 1 <= 0) {
+        cnt2 = -2;
+      } else if (b - 1 == mn) {
+        cnt2++;
+      }
 
-      console.log(cnt);
+      if (cnt1 < 0 && cnt2 < 0) {
+        console.log(-1);
+      } else {
+        if (cnt1 < 0) ans = cnt2;
+        else if (cnt2 < 0) ans = cnt1;
+        else ans = Math.min(cnt1, cnt2);
+        console.log(ans);
+      }
     }
   }
 });
